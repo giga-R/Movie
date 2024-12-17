3a4052ed1a9054101fabbb93775d222e4ddd7e27
giga-R <rafiamjad12@gmail.com>
2024-12-17 11:20:11 +0530

    login created

--- app/src/main/java/com/example/movieapp/MainActivity.java
+++ app/src/main/java/com/example/movieapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.movieapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +11,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1;
+    EditText e1,e2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        b1=(Button) findViewById(R.id.logbut);
+        e1=(EditText) findViewById(R.id.uname);
+        e2=(EditText) findViewById(R.id.pass);
     }
 }
